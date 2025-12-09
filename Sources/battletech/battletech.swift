e951f4ef2a25d99054bf3a36a126d090d8c7124e
Richard Burgess <rickb@rickb.com>
2025-12-08 20:48:15 -0600

    spreadsheet

--- Sources/battletech/battletech.swift
+++ Sources/battletech/battletech.swift
@@ -1,14 +1,88 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-// 
-// Swift Argument Parser
-// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
-
 import ArgumentParser
+import Foundation
 
 @main
-struct battletech: ParsableCommand {
-    mutating func run() throws {
-        print("Hello, world!")
+struct BattleTech: ParsableCommand {
+
+    @Option(help: "weapons directory")
+    var weapons: [String]
+
+    func run() throws {
+        let files = try weapons.map { path in
+            (try FileManager.default.subpathsOfDirectory(atPath: path)).map {
+                path + "/" + $0
+            }
+        }.flatMap {
+            $0
+        }.filter {
+            $0.hasSuffix(".json")
+        }
+        var spreadsheet = ""
+
+        spreadsheet += [
+            "Name",
+            "Category",
+            "Type",
+            "Sub Type",
+            "Damage",
+            "Tonnage",
+            "Heat"
+        ].joined(separator: "\t") + "\n"
+
+        files.forEach { file in
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: file))
+                let weapon = try JSONDecoder().decode(Weapon.self, from: data)
+                spreadsheet += [
+                    weapon.name,
+                    weapon.Category,
+                    weapon.WeaponType,
+                    weapon.WeaponSubType,
+                    String(weapon.Damage),
+                    String(weapon.Tonnage),
+                    String(weapon.HeatGenerated)
+                ].joined(separator: "\t") + "\n"
+            } catch {
+            }
+        }
+
+        try spreadsheet.data(using: .utf8)?.write(to: URL(fileURLWithPath: "out.tsv"))
+    }
+}
+
+struct Weapon: Decodable {
+    let Category: String
+    let WeaponType: String
+    let WeaponSubType: String
+    let HeatGenerated: Float
+    let Damage: Float
+    let Tonnage: Float
+    let BonusValueA: String
+    let BonusValueB: String
+    let Description: DescriptionInfo
+
+    enum CodingKeys: String, CodingKey {
+        case Category
+        case WeaponType = "Type"
+        case WeaponSubType
+        case HeatGenerated
+        case Damage
+        case Tonnage
+        case BonusValueA
+        case BonusValueB
+        case Description
+    }
+
+    struct DescriptionInfo: Decodable {
+        let Id: String
+
+    }
+
+    var name: String {
+        return [
+            Description.Id, BonusValueA, BonusValueB
+        ].compactMap {
+            $0.isEmpty ? nil : $0
+        }.joined(separator: "")
     }
 }
