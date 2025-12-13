254b0adf7be9702247fb16dbc691c3e1041a9a8e
Richard Burgess <rickb@rickb.com>
2025-12-12 23:37:41 -0600

    update

---
+++ Sources/battletech/Weapon.swift
@@ -0,0 +1,172 @@
+//
+//  File.swift
+//  battletech
+//
+//  Created by Richard Burgess on 12/12/25.
+//
+
+import Foundation
+
+let doubleClanHeatSinkPerTon = 12.0
+
+let ammoWeight = [
+    "MG": 1.0 / 200.0,
+    "SRM": 1.0 / 100.0,
+    "LRM": 1.0 / 120.0,
+    "AC2": 1.0 / 25.0,
+    "AC5": 1.0 / 15.0,
+    "AC10": 1.0 / 8.0,
+    "AC20": 1.0 / 5.0,
+    "GAUSS": 1.0 / 8.0,
+    "LB2X": 1.0 / 25.0,
+    "LB5X": 1.0 / 15.0,
+    "LB10X": 1.0 / 8.0,
+    "LB20X": 1.0 / 5.0
+]
+struct Weapon: Decodable & SpreadsheetConvertible{
+    let category: String
+    let weaponType: String
+    let weaponSubType: String
+    let heatGenerated: Double
+    let damage: Double
+    let tonnage: Double
+    let bonusValueA: String
+    let bonusValueB: String
+    let description: Description
+    let shotsWhenFired: Double
+    let maxRange: Int
+    let ammoCategoryID: String
+
+    var bonus: String {
+        [bonusValueA, bonusValueB].joined(separator: " ")
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case category = "Category"
+        case weaponType = "Type"
+        case weaponSubType = "WeaponSubType"
+        case heatGenerated = "HeatGenerated"
+        case damage = "Damage"
+        case tonnage = "Tonnage"
+        case bonusValueA = "BonusValueA"
+        case bonusValueB = "BonusValueB"
+        case description = "Description"
+        case shotsWhenFired = "ShotsWhenFired"
+        case maxRange = "MaxRange"
+        case ammoCategoryID
+    }
+
+    struct Description: Decodable {
+        let uiName: String
+
+        enum CodingKeys: String, CodingKey {
+            case uiName = "UIName"
+        }
+    }
+
+    var totalDamage: Double {
+        damage * shotsWhenFired
+    }
+
+    func effectiveTonnage(heatSinkPerTon: Double, rounds: Double) -> Double {
+        tonnage + (heatGenerated / heatSinkPerTon) + ammoTonnage(rounds: rounds)
+    }
+
+    func ammoTonnage(rounds: Double) -> Double {
+        guard let weight = ammoWeight[ammoCategoryID] else {
+            return 0
+        }
+        return rounds * shotsWhenFired * weight
+    }
+
+    var displayDamagePerHeat: String {
+        heatGenerated == 0 ? "No Heat" : String(format: "%.1f", totalDamage / heatGenerated)
+    }
+
+    var displayDamagePerTon: String {
+        String(format: "%.1f", totalDamage / tonnage)
+    }
+
+    var displayDamage: String {
+        String(totalDamage)
+    }
+
+    var displayTonnage: String {
+        String(tonnage)
+    }
+
+    var displayHeatGenerated: String {
+        String(heatGenerated)
+    }
+
+    var displayMaxRange: String {
+        String(maxRange)
+    }
+
+    func effectiveDamagePerTon(
+        heatSinkPerTon: Double = doubleClanHeatSinkPerTon,
+        rounds: Double = 10
+    ) -> Double {
+        totalDamage / effectiveTonnage(heatSinkPerTon: heatSinkPerTon, rounds: rounds)
+    }
+
+    var displayEffectiveDamagePerTon: String {
+        String(format: "%.1f", effectiveDamagePerTon())
+    }
+
+    var displayName: String {
+        description.uiName
+    }
+
+    static func header() -> String {
+        [
+            "Name",
+            "Category",
+            "Type",
+            "Sub Type",
+            "Range",
+            "Damage",
+            "Tonnage",
+            "Heat",
+            "Dmg/Ton",
+            "Eff Dmg/Ton",
+            "Dmg/Heat",
+            "Bonus"
+        ].joined(separator: "\t")
+    }
+
+    func toRow() -> String {
+        [
+            displayName,
+            category,
+            weaponType,
+            weaponSubType,
+            displayMaxRange,
+            displayDamage,
+            displayTonnage,
+            displayHeatGenerated,
+            displayDamagePerTon,
+            displayEffectiveDamagePerTon,
+            displayDamagePerHeat,
+            bonus,
+        ]
+        .joined(separator: "\t")
+    }
+}
+
+extension BattleTech {
+    func parseWeapons() throws {
+        guard weapons.isEmpty == false else {
+            return
+        }
+
+        let files = try jsonPaths(from: weapons)
+
+        try (
+            [Weapon.header()] +
+            convert(files: files, type: Weapon.self)
+        )
+        .joined(separator: "\n")
+        .data(using: .utf8)?.write(to: URL(fileURLWithPath: "weapons.tsv"))
+    }
+}
--- Sources/battletech/battletech.swift
+++ Sources/battletech/battletech.swift
@@ -7,114 +7,42 @@ struct BattleTech: ParsableCommand {
     @Option(help: "weapons directory")
     var weapons: [String]
 
+    @Option(help: "mechs directory")
+    var mechs: [String]
+
     func run() throws {
-        let files = try weapons.map { path in
-            (try FileManager.default.subpathsOfDirectory(atPath: path)).map {
-                path + "/" + $0
-            }
-        }.flatMap {
-            $0
-        }.filter {
-            $0.hasSuffix(".json")
-        }
-        var spreadsheet = ""
-
-        spreadsheet += [
-            "Name",
-            "Category",
-            "Type",
-            "Sub Type",
-            "Range",
-            "Damage",
-            "Tonnage",
-            "Heat",
-            "Bonus",
-            "Dmg/Ton",
-            "Dmg/Heat"
-        ].joined(separator: "\t") + "\n"
-
-        files.forEach { file in
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: file))
-                let weapon = try JSONDecoder().decode(Weapon.self, from: data)
-                spreadsheet += [
-                    weapon.Description.UIName,
-                    weapon.Category,
-                    weapon.WeaponType,
-                    weapon.WeaponSubType,
-                    String(weapon.MaxRange),
-                    weapon.displayDamage,
-                    weapon.displayTonnage,
-                    weapon.displayHeatGenerated,
-                    weapon.bonus,
-                    weapon.displayDamagePerTon,
-                    weapon.displayDamagePerHeat
-                ].joined(separator: "\t") + "\n"
-            } catch {
-            }
-        }
-
-        try spreadsheet.data(using: .utf8)?.write(to: URL(fileURLWithPath: "weapons.tsv"))
+        try parseWeapons()
+        try parseMechs()
     }
 }
 
-struct Weapon: Decodable {
-    let Category: String
-    let WeaponType: String
-    let WeaponSubType: String
-    let HeatGenerated: Float
-    let Damage: Float
-    let Tonnage: Float
-    let BonusValueA: String
-    let BonusValueB: String
-    let Description: DescriptionInfo
-    let ShotsWhenFired: Float
-    let MaxRange: Int
+protocol SpreadsheetConvertible {
+    static func header() -> String
+    func toRow() -> String
+}
 
-    var bonus: String {
-        [BonusValueA, BonusValueB].joined(separator: " ")
+func convert<T: Decodable & SpreadsheetConvertible>(files: [String], type: T.Type) -> [String] {
+    files.compactMap { file in
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: file))
+            return try JSONDecoder().decode(T.self, from: data).toRow()
+        } catch {
+            return nil
+        }
     }
-    enum CodingKeys: String, CodingKey {
-        case Category
-        case WeaponType = "Type"
-        case WeaponSubType
-        case HeatGenerated
-        case Damage
-        case Tonnage
-        case BonusValueA
-        case BonusValueB
-        case Description
-        case ShotsWhenFired
-        case MaxRange
+}
+
+func jsonPaths(from paths: [String]) throws -> [String] {
+    try paths.map { path in
+        (try FileManager.default.subpathsOfDirectory(atPath: path)).map {
+            path + "/" + $0
+        }
+    }.flatMap {
+        $0
+    }.filter {
+        $0.hasSuffix(".json")
     }
 
-    struct DescriptionInfo: Decodable {
-        let UIName: String
-    }
-
-    var totalDamage: Float {
-        Damage * ShotsWhenFired
-    }
-
-    var displayDamagePerHeat: String {
-        HeatGenerated == 0 ? "No Heat" : String(format: "%.1f", totalDamage / HeatGenerated)
-    }
-
-    var displayDamagePerTon: String {
-        String(format: "%.1f", totalDamage / Tonnage)
-    }
-
-    var displayDamage: String {
-        String(totalDamage)
-    }
-
-    var displayTonnage: String {
-        String(Tonnage)
-    }
-
-    var displayHeatGenerated: String {
-        String(HeatGenerated)
-    }
 }
 
 extension String {
@@ -123,6 +51,6 @@ extension String {
     }
 }
 
-// swift run battletech --weapons "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Data/StreamingAssets/data/weapon" --weapons "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Mods/Expanded Arsenal-635-5-0-3-1687981803/ExpandedArsenal/weapons" --weapons "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Mods/Expanded Arsenal-635-5-0-3-1687981803/EliteArsenal/weapons"
+// swift run battletech --weapons "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Data/StreamingAssets/data/weapon" --weapons "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Mods/Expanded Arsenal-635-5-0-3-1687981803/ExpandedArsenal/weapons" --weapons "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Mods/Expanded Arsenal-635-5-0-3-1687981803/EliteArsenal/weapons" --mechs "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Data/StreamingAssets/data/mech" --mechs "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Mods/Expanded Arsenal-635-5-0-3-1687981803/ExpandedArsenal/mech" --mechs "/Volumes/Developer/SteamLibrary/steamapps/common/BATTLETECH/BattleTech.app/Contents/Resources/Mods/Expanded Arsenal-635-5-0-3-1687981803/EliteArsenal/mech"
 
 
